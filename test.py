4a7f2111b84d790819dcbb75319772b797534787
codefuldom <dominiquehallan@gmail.com>
2019-04-29 16:59:09 -0500

    :tada: running python from cli

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ age = 5
 print(age * 365 * 24 * 60 * 60)
 
 seconds_alive = age * 365 * 24 * 60 * 60
-print(f'You have lived for {seconds_alive} seconds.')
+print(f'You have lived for {seconds_alive} seconds. This is {age} years.')
