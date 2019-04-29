786933bb369933816aaaa1fb0afd94a4cdfeab1e
codefuldom <dominiquehallan@gmail.com>
2019-04-29 18:29:39 -0500

    :tada: manipulating lists in python

---
+++ numbers.py
@@ -0,0 +1,11 @@
+names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(len(names))
+print(names[-1])
+
+nums = [1, 2, 3, 4]
+print(len(nums))
+
+print(len(nums) - 1)
+print(names[10])
+
+print(f'The last number in the names array is {len(names) - 1}')
--- test.py
+++ test.py
@@ -2,3 +2,6 @@ age = int(input('Enter your age please: '))
 
 seconds_alive = age * 365 * 24 * 60 * 60
 print(f'You have lived for {seconds_alive} seconds. This is {age} years.')
+
+numbers = [4, 5, 6, 7]
+print(numbers)
