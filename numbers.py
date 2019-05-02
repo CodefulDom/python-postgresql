42a1173f21647e30b1a80cad02b35682f26aa17a
codefuldom <dominiquehallan@gmail.com>
2019-05-01 21:51:34 -0500

    :tada: module WIP

--- numbers.py
+++ numbers.py
@@ -1,11 +1,17 @@
-names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(len(names))
-print(names[-1])
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(len(numbers))
+print(numbers[-1])
 
 nums = [1, 2, 3, 4]
 print(len(nums))
 
 print(len(nums) - 1)
-print(names[10])
+print(numbers[10])
 
-print(f'The last number in the names array is {len(names) - 1}')
+print(f"The last number in the numbers array is {len(numbers) - 1}")
+
+for number in numbers:
+    print(number)
+
+for number in numbers:
+    print(number ** 2)
