513d3b2bca649b9faba07c7bc2102e0b300e3886
codefuldom <dominiquehallan@gmail.com>
2019-05-01 23:24:07 -0500

    :tada: module WIP

--- booleans.py
+++ booleans.py
@@ -9,3 +9,15 @@ numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 for number in numbers_array:
     if number > 5:
         print(number)
+
+print(10 in numbers_array)
+print(10 not in numbers_array)
+
+magic_number = [3, 9]
+user_number = 3
+print(user_number in magic_number)
+
+if user_number == 3 or user_number == 9:
+    print(f"Your guess of {user_number} was correct")
+else:
+    print(f"Your number of {user_number} was incorrect.")
